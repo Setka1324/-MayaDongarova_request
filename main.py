92ce21a99839e92deaad756f0df6ec79661c3a64
Mark <47208369+Setka1324@users.noreply.github.com>
2022-07-01 22:03:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-import openpyxl
 
 stations = ['aberporth','armagh','ballypatrick','bradford','braemar', 'camborne', 'cambridge', 'cardiff', 'chivenor', 'cwmystwyth', 'dunstaffnage', 'durham', 'eastbourne', 'eskdalemuir', 'heathrow', 'hurn','lerwick','leuchars','lowestoft','manston','nairn','newtonrigg','oxford','paisley','ringway','rossonwye','shawbury','sheffield','southampton','stornoway','suttonbonington','tiree','valley','waddington','whitby','wickairport','yeovilton']
 
